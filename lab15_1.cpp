e05e093821f7374ac060f5e832ccdb6b17676507
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-02-05 18:01:08 +0700

    Update lab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void findColSum(const double *dPtr,double *sum2,int N ,int M){
+	for (int k = 0; k < M; k++){
+		*(sum2+k) = 0;
+	}	
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < M; j++){
+				*(sum2+j) += *(dPtr+i*M+j);	
+		}
+	}
+	
+}
+void showData(double *dPtr,int N , int M){
+		cout << fixed << setprecision(2);
+    	for(int i = 0; i < N*M; i++){
+        cout <<dPtr[i];
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double *dPtr,int N ,int M){
+	for (int i = 0; i < N*M; i++){
+		*(dPtr+i) =(rand()%101)/100.0;
+	}
+	
+}
+void findRowSum(const double *dPtr,double *sum1,int N,int M){
+	for (int i = 0; i < N; i++){
+		*(sum1+i) =0; 
+		for (int j = 0; j < M; j++){
+			*(sum1+i) += *(dPtr+i*M+j);
+		}
+		
+	}
+	
+}
